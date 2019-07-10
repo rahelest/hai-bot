4d557b2235095fa1c9beed4511eada6af2576a63
Rahel Piip <rahel.piip@roguewave.com>
2019-07-10 07:46:38 +0300

    Add new clean command.

--- events/message.js
+++ events/message.js
@@ -1,7 +1,11 @@
+const clean = require("../commands/clean");
 const kick = require("../commands/kick");
 
 module.exports = (client, message) => {
   if (message.content.startsWith("!kick")) {
     return kick(message);
+  }
+  if (message.content.startsWith("!clean")) {
+    return clean(message);
   }
 };
